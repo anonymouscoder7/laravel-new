0a080d9062a10c871813d9b7092237584e2b0d55
anonymouscoder7 <lpsubedi57@gmail.com>
2024-04-03 21:11:59 +0545

    push

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontendController extends Controller
+{
+    function index() {
+        $name = 'ram';
+        $contact = 9862220799;
+        return view('index',compact('name','contact'));
+    }
+    function about($id){
+        dd($id);
+        return view('frontend.about');
+    }
+    function contact(){
+        return view('frontend.contact');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\FrontendController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/',[FrontendController::class,'index']);
+Route::get('/about/{id}',[FrontendController::class,'about']);
+Route::get('/contact',[FrontendController::class,'contact']);
+
+
+
+
+
+
